601a39601cab3aea0e9474ad05d08fb363f9cc42
Turquand Maud <turquand@georgette.cirad.fr>
2020-09-14 11:37:13 -0300

    Script Markdown basic

---
+++ installation_cours_markdown.R
@@ -0,0 +1,37 @@
+Library <- function(Packages) {
+  InstallAndLoad <- function(Package) {
+    if (!Package %in% installed.packages()[, 1]) {install.packages(Package, repos="https://cran.rstudio.com/")}
+    require(Package, character.only = TRUE)
+  }
+  invisible(sapply(Packages, InstallAndLoad))
+}
+# Ajouter les packages nécessaires ici
+Library(c("rmarkdown", "bookdown","knitr"))
+
+# télécharger et installer MikTex selon votre susytème d'exploitation
+# https://miktex.org/download
+# petite modif d'essai sur Git 
+
+
+# télécharger et installer Rtools depuis le  site
+#https://cran.r-project.org/bin/windows/Rtools/
+  
+
+# quitter et redémarrer Rstudio
+
+Sys.which("make") ## si cette commande doit ceci "C:\\rtools40\\usr\\bin\\make.exe"
+
+
+install.packages("devtools")
+library("devtools")
+install_github("EcoFoG/EcoFoG")
+
+
+install.packages('tinytex') # tinytex permet une mise à jour de latex avant chaque tricotages
+tinytex::install_tinytex()  # install TinyTeX
+
+library(rmarkdown)
+library(bookdown)
+
+
+library("EcoFoG")
